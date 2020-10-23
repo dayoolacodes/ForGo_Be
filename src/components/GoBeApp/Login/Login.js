7694e1895ec8cedbb0b47b9e4ffe918451a5bacd
dayoola <ayodayo1@gmail.com>
2020-10-23 13:06:28 +0100

    login page done

--- src/components/GoBeApp/Login/Login.js
+++ src/components/GoBeApp/Login/Login.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import Aux from '../../../hoc/Auxil'
 import classes from './Login.module.css'
+import illustration1 from '../../../assets/images/illustration1.svg'
 
 
-const login = (prop) => {
+
+const Login = (prop) => {
+//    const  [check, setCheck] = useState(false)
+
+//    const handleCheck =()=>{
+//        setCheck({check: !check})
+//    }
+
     return ( 
         <Aux className={classes.Login}>
-            <Container>
-                <Row>
-                <div className={classes.Text}>
-                    <p>
-                    Get access to valuable resources and information to grow a career in education and collaborate with passionate educators just like you.
-                    </p>
-                <input placeholder='First Name' className={classes.Form} />
-                <input placeholder='Email' className={classes.Form} />
-                </div>
-                 </Row>
-            </Container>
+          
+                <div className={classes.Text}> 
+                
+                 Get access to valuable resources and information to grow a career in education and collaborate with passionate educators just like you.
+                <input placeholder='First Name' type='name' className={classes.TextBox} style={{marginTop: "80px"}}/>
+                <input placeholder='Email' type='email' className={classes.TextBox} />
+                <input type="checkbox"  name="subscribe" style={{marginLeft: "20px"}}/> 
+                <span className={classes.SubscribeText} >Subscribe to our newsletter</span>
+                <button className={classes.Button} type='submit'> Submit </button>
+                  </div>  
+                  <div style={{width:"800px", margin: "100px"}}>
+                    <img src={illustration1} alt='ils'/>
+                  </div>
+
+                
+             
         </Aux>
      );
 }
  
-export default login;
+export default Login;
